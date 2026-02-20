cd94c721d468f00c5665951663661f3369ee5e5a
Ammar-25 <2023170376@cis.asu.edu.eg>
2026-02-20 14:33:47 +0200

    UI updated

--- script.js
+++ script.js
@@ -17,4 +17,28 @@ document.querySelectorAll("section").forEach((section) => {
 function toggle() {
   const navBar = document.querySelector(".navBar");
   navBar.classList.toggle("mobile-active");
+  const menuToggle = document.querySelector(".menu-toggle i");
+  if (navBar.classList.contains("mobile-active")) {
+    menuToggle.classList.remove("fa-bars");
+    menuToggle.classList.add("fa-xmark");
+  } else {
+    menuToggle.classList.remove("fa-xmark");
+    menuToggle.classList.add("fa-bars");
+  }
 }
+const text = "Hello, I'm Ammar Mohamed Ali.";
+let index = 0;
+function typeEffect() {
+  if (index < text.length) {
+    document.getElementById("name").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeEffect, 100);
+  } else {
+    setTimeout(() => {
+      document.getElementById("name").innerHTML = "Hello,";
+      index = 6;
+      typeEffect();
+    }, 2000);
+  }
+}
+typeEffect();
